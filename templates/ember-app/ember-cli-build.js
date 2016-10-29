ccb06430dbe3a5654a4e5f0888b09e5273116b9f
Shogun <shogun@cowtech.it>
2016-10-29 12:42:46 +0200

    Minor fixes.

--- templates/ember-app/ember-cli-build.js
+++ templates/ember-app/ember-cli-build.js
@@ -4,7 +4,7 @@ const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function(defaults){
   const deploying = (/^(production|staging)/).test(EmberApp.env());
-  
+
   const app = new EmberApp(defaults, {
     tests: false,
     sassOptions: {
@@ -15,9 +15,9 @@ module.exports = function(defaults){
     minifyCSS: {enabled: deploying},
     gzip: {enabled: deploying, keepUncompressed: true, extensions: ["js", "css", "jpg", "png", "bmp", "svg", "eot", "otf", "ttf", "woff", "woff2"]}
   });
-  
+
   // Utility libraries
   [].forEach(e => app.import(e));
-  
+
   return app.toTree();
 };
