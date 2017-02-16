c2f4f8450c2e7c4a9bd68f5a117bae1319f0c20e
Shogun <shogun@cowtech.it>
2017-02-16 09:17:54 +0100

    Fix NPM package main file.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### 2016-02-16 / 5.0.1
+
+* Fix NPM package main file.
+
 ### 2016-02-08 / 5.0.0
 
 * Updated minimum Node.js version to 7.
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony-async-await
 const cli = require("commander");
 const bluebird = require("bluebird");
 const fs = bluebird.promisifyAll(require("fs-extra"));
